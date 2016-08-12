7d97bb5c3a270f5df36f64937bc087b75c80ea2a
Aluan Haddad <aluanh@gmail.com>
2016-08-11 22:46:12 -0400

    Begin testing different --m targets

--- index.js
+++ index.js
@@ -106,4 +106,3 @@ function snakeCase(identifier) {
         .map(segment => segment.toLowerCase())
         .join('-');
 }
-//# sourceMappingURL=index.js.map
--- tests/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-require('./input');
-require('./component');
-//# sourceMappingURL=index.js.map
