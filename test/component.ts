608ee31b1fec3c2434fcb12317903ed96b62e0f5
Aluan Haddad <aluanh@gmail.com>
2017-01-03 00:51:45 -0500

    2.0.1 --noImplicitAny

--- test/component.ts
+++ test/component.ts
@@ -1,146 +1,146 @@
 import 'reflect-metadata';
 import test = require('tape');
 import {
-    component,
-    ComponentOptions
+  component,
+  ComponentOptions
 } from '../index';
 import {
-    AnimationEntryMetadata,
-    ChangeDetectionStrategy,
-    ViewEncapsulation
+  AnimationEntryMetadata,
+  ChangeDetectionStrategy,
+  ViewEncapsulation
 } from '@angular/core';
 import extractMetadata from './helpers/extract-metadata';
 
 const template = '<div></div>';
 
 test('@component creates kebab-cased selector excluding "Component" suffix', ({ equal, end }) => {
-    @component(template) class DynamicListViewComponent { }
+  @component(template) class DynamicListViewComponent { }
 
-    const metadata = extractMetadata(DynamicListViewComponent);
+  const metadata = extractMetadata(DynamicListViewComponent);
 
-    equal(metadata.annotations[0].selector, 'dynamic-list-view');
+  equal(metadata.annotations[0].selector, 'dynamic-list-view');
 
-    end();
+  end();
 });
 
 test('@component creates full selector if component suffix is absent from class name', ({ equal, end }) => {
-    @component(template) class DynamicListView { }
+  @component(template) class DynamicListView { }
 
-    const metadata = extractMetadata(DynamicListView);
+  const metadata = extractMetadata(DynamicListView);
 
-    equal(metadata.annotations[0].selector, 'dynamic-list-view');
+  equal(metadata.annotations[0].selector, 'dynamic-list-view');
 
-    end();
+  end();
 });
 
 test('@component propagates template to metadata when it is the sole argument', ({ equal, end }) => {
-    @component(template) class DynamicListView { }
+  @component(template) class DynamicListView { }
 
-    const metadata = extractMetadata(DynamicListView);
-    equal(metadata.annotations[0].template, template);
+  const metadata = extractMetadata(DynamicListView);
+  equal(metadata.annotations[0].template, template);
 
-    end();
+  end();
 });
 
 test('@component propagates template to metadata when it is the first of 2 arguments', ({ equal, end }) => {
 
-    @component(template, {}) class DynamicListView { }
+  @component(template, {}) class DynamicListView { }
 
-    const metadata = extractMetadata(DynamicListView);
+  const metadata = extractMetadata(DynamicListView);
 
-    equal(metadata.annotations[0].template, template);
+  equal(metadata.annotations[0].template, template);
 
-    end();
+  end();
 });
 
-test('@component propagates template to metadata when specified as the first argument of 3 arguments', ({deepEqual, end}) => {
-    const style = 'h1 { background:"aqua"; }';
-    @component(template, style, { providers: [class { }] }) class DynamicListView { }
+test('@component propagates template to metadata when specified as the first argument of 3 arguments', ({ deepEqual, end }) => {
+  const style = 'h1 { background:"aqua"; }';
+  @component(template, style, { providers: [class { }] }) class DynamicListView { }
 
-    const metadataKeys = Reflect.getMetadataKeys(DynamicListView);
-    const metadata = metadataKeys.reduce((metadata, key) => {
-        metadata[key] = Reflect.getMetadata(key, DynamicListView);
-        return metadata;
-    }, {});
+  const metadataKeys = Reflect.getMetadataKeys(DynamicListView);
+  const metadata = metadataKeys.reduce((metadata, key) => {
+    metadata[key] = Reflect.getMetadata(key, DynamicListView);
+    return metadata;
+  }, {});
 
-    deepEqual(metadata.annotations[0].template, template);
+  deepEqual(metadata.annotations[0].template, template);
 
-    end();
+  end();
 });
 
-test('@component propagates style to styles metadata when specified as the 2nd of 2 arguments', ({deepEqual, end}) => {
-    const style = 'h1 { background: "aqua"; }';
+test('@component propagates style to styles metadata when specified as the 2nd of 2 arguments', ({ deepEqual, end }) => {
+  const style = 'h1 { background: "aqua"; }';
 
-    @component('<div></div>', style) class DynamicListView { }
+  @component('<div></div>', style) class DynamicListView { }
 
-    const metadata = extractMetadata(DynamicListView);
+  const metadata = extractMetadata(DynamicListView);
 
-    deepEqual(metadata.annotations[0].styles, [style]);
+  deepEqual(metadata.annotations[0].styles, [style]);
 
-    end();
+  end();
 });
 
-test('@component propagates style to styles metadata when specified as the 2nd of 3 arguments', ({deepEqual, end}) => {
-    const style = 'h1 { background: "aqua"; }';
+test('@component propagates style to styles metadata when specified as the 2nd of 3 arguments', ({ deepEqual, end }) => {
+  const style = 'h1 { background: "aqua"; }';
 
-    @component(template, style, { providers: [class { }] }) class DynamicListView { }
+  @component(template, style, { providers: [class { }] }) class DynamicListView { }
 
-    const metadata = extractMetadata(DynamicListView);
+  const metadata = extractMetadata(DynamicListView);
 
-    deepEqual(metadata.annotations[0].styles, [style]);
+  deepEqual(metadata.annotations[0].styles, [style]);
 
-    end();
+  end();
 });
 
-test('@component propagates all options to metadata when options is the 2nd of 2 arguments', ({deepEqual, end}) => {
-    const componentOptions = createPopulatedComponentOptions();
+test('@component propagates all options to metadata when options is the 2nd of 2 arguments', ({ deepEqual, end }) => {
+  const componentOptions = createPopulatedComponentOptions();
 
-    @component(template, componentOptions) class DynamicListView { }
+  @component(template, componentOptions) class DynamicListView { }
 
-    const metadata = extractMetadata(DynamicListView);
+  const metadata = extractMetadata(DynamicListView);
 
-    Object.keys(componentOptions).forEach(key => {
-        console.info(key);
-        const target = metadata.annotations[0][key] === 0 ? 0 : metadata.annotations[0][key] || metadata.annotations[0][`_${key}`];
-        deepEqual(componentOptions[key], target);
-    });
+  Object.keys(componentOptions).forEach((key: keyof extractMetadata.Metadata.Annotation) => {
+    console.info(key);
+    const target = metadata.annotations[0][key] === 0 ? 0 : metadata.annotations[0][key] || metadata.annotations[0][`_${key}`];
+    deepEqual(componentOptions[key], target);
+  });
 
-    end();
+  end();
 });
 
-test('@component propagates all options to metadata when options is the 3rd of 3 arguments', ({deepEqual, end}) => {
-    const style = 'h1 { background:"aqua"; }';
-    const template = '<div></div>';
+test('@component propagates all options to metadata when options is the 3rd of 3 arguments', ({ deepEqual, end }) => {
+  const style = 'h1 { background:"aqua"; }';
+  const template = '<div></div>';
 
-    const componentOptions = createPopulatedComponentOptions();
+  const componentOptions = createPopulatedComponentOptions();
 
-    @component(template, style, componentOptions) class DynamicListView { }
+  @component(template, style, componentOptions) class DynamicListView { }
 
-    const metadata = extractMetadata(DynamicListView);
+  const metadata = extractMetadata(DynamicListView);
 
-    Object.keys(componentOptions).forEach((key, index) => {
-        console.info(key);
-        const target = metadata.annotations[0][key] === 0 ? 0 : metadata.annotations[0][key] || metadata.annotations[0][`_${key}`];
-        deepEqual(componentOptions[key], target);
-    });
+  Object.keys(componentOptions).forEach((key: keyof extractMetadata.Metadata.Annotation) => {
+    console.info(key);
+    const target = metadata.annotations[0][key] === 0 ? 0 : metadata.annotations[0][key] || metadata.annotations[0][`_${key}`];
+    deepEqual(componentOptions[key], target);
+  });
 
-    end();
+  end();
 });
 
-function createPopulatedComponentOptions(): ComponentOptions {
-    return {
-        animations: [new AnimationEntryMetadata('a', [])],
-        changeDetection: ChangeDetectionStrategy.OnPush,
-        encapsulation: ViewEncapsulation.None,
-        exportAs: 'anonymous',
-        host: { 'app': 'src' },
-        interpolation: ['x', 'y'],
-        moduleId: 'somewhere',
-        entryComponents: [class { }],
-        providers: [{ provide: 'SomeService', useClass: class Provider { }, multi: false }, [{
-            provide: 'SomeServiceViaFactory', useFactory: (dep1) => dep1 + 2, deps: [1]
-        }]],
-        queries: { awake: 'always' },
-        viewProviders: [class { }, [class { }]]
-    };
+function createPopulatedComponentOptions(): ComponentOptions & { [key: string]: {} | {}[] } {
+  return {
+    animations: [new AnimationEntryMetadata('a', [])],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    encapsulation: ViewEncapsulation.None,
+    exportAs: 'anonymous',
+    host: { 'app': 'src' },
+    interpolation: ['x', 'y'],
+    moduleId: 'somewhere',
+    entryComponents: [class { }],
+    providers: [{ provide: 'SomeService', useClass: class Provider { }, multi: false }, [{
+      provide: 'SomeServiceViaFactory', useFactory: (dep1: number) => dep1 + 2, deps: [1]
+    }]],
+    queries: { awake: 'always' },
+    viewProviders: [class { }, [class { }]]
+  };
 }
--- test/helpers/extract-metadata.ts
+++ test/helpers/extract-metadata.ts
@@ -1,4 +1,4 @@
-export default function (Decorated): { annotations?: { selector?: string, template?: string, styles?: string[], exportAs?: string }[] } {
+export function extractMetadata(Decorated: {}): extractMetadata.Metadata {
   const metadataKeys = Reflect.getMetadataKeys(Decorated);
   const metadata = metadataKeys.reduce((metadata, key) => {
     metadata[key] = Reflect.getMetadata(key, Decorated);
@@ -6,3 +6,23 @@ export default function (Decorated): { annotations?: { selector?: string, templa
   }, {});
   return metadata;
 }
+
+export declare namespace extractMetadata {
+  interface Data {
+    annotations?: (Metadata.Annotation & {
+      [P in keyof Metadata.Annotation]?: Metadata.Annotation[P]})[];
+  }
+  export type Metadata = {
+    [P in keyof extractMetadata.Data]: Data[P]
+  } & {
+      annotations?: (Metadata.Annotation & { [key: string]: {} })[];
+    };
+  export namespace Metadata {
+    export interface Annotation {
+      encapsulation: number;
+      selector?: string; template?: string; styles?: string[]; exportAs?: string;
+    }
+  }
+}
+
+export default extractMetadata;
